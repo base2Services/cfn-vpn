541939813986ae124e19846585f78b26e24c2503
Guslington <guslington@gmail.com>
2019-06-27 09:26:49 +1000

    add parameters into change set

--- lib/cfnvpn/cloudformation.rb
+++ lib/cfnvpn/cloudformation.rb
@@ -29,14 +29,29 @@ module CfnVpn
       return does_cf_stack_exist() ? 'UPDATE' : 'CREATE'
     end
 
-    def create_change_set(template_path)
-      change_set_name = "#{@stack_name}-#{CfnVpn::CHANGE_SET_VERSION}"
+    def create_change_set(template_path,parameters)
+      change_set_name = "#{@stack_name}-#{CfnVpn::CHANGE_SET_VERSION}-#{Time.now.utc.strftime("%Y%m%d%H%M%S")}"
       change_set_type = get_change_set_type()
+
+      if change_set_type == 'CREATE'
+        params = get_parameters_from_template(template_path)
+      else
+        params = get_parameters_from_stack()
+      end
+
+      params.each do |param|
+        if !parameters[param[:parameter_key]].nil?
+          param['parameter_value'] = parameters[param[:parameter_key]]
+          param['use_previous_value'] = false
+        end
+      end
+
       template_body = File.read(template_path)
       Log.logger.debug "Creating changeset"
       change_set = @client.create_change_set({
         stack_name: @stack_name,
         template_body: template_body,
+        parameters: params,
         tags: [
           {
             key: "cfnvpn:version",
@@ -55,7 +70,19 @@ module CfnVpn
 
     def wait_for_changeset(change_set_id)
       Log.logger.debug "Waiting for changeset to be created"
-      @client.wait_until :change_set_create_complete, change_set_name: change_set_id
+      begin
+        @client.wait_until :change_set_create_complete, change_set_name: change_set_id
+      rescue Aws::Waiters::Errors::FailureStateError => e
+        change_set = get_change_set(change_set_id)
+        Log.logger.error("change set status: #{change_set.status} reason: #{change_set.status_reason}")
+        exit 1
+      end
+    end
+
+    def get_change_set(change_set_id)
+      @client.describe_change_set({
+        change_set_name: change_set_id,
+      })
     end
 
     def execute_change_set(change_set_id)
@@ -69,6 +96,17 @@ module CfnVpn
       waiter = change_set_type == 'CREATE' ? :stack_create_complete : :stack_update_complete
       Log.logger.info "Waiting for changeset to #{change_set_type}"
       resp = @client.wait_until waiter, stack_name: @stack_name
+    end
+
+    def get_parameters_from_stack()
+      resp = @client.get_template_summary({ stack_name: @stack_name })
+      return resp.parameters.collect { |p| { parameter_key: p.parameter_key, use_previous_value: true }  }
+    end
+
+    def get_parameters_from_template(template_path)
+      template_body = File.read(template_path)
+      resp = @client.get_template_summary({ template_body: template_body })
+      return resp.parameters.collect { |p| { parameter_key: p.parameter_key, parameter_value: p.default_value }  }
     end
 
   end
